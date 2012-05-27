d39ff25b0a84d072b25bc9e5736eff2ab4526f90
Steven Myint <git@stevenmyint.com>
2012-05-26 18:36:38 -0700

    Move main back to docformatter.py

--- docformatter.py
+++ docformatter.py
@@ -1,5 +1,6 @@
 """Formats docstrings to follow PEP 257."""
 
+import sys
 import re
 import tokenize
 try:
@@ -118,3 +119,38 @@ def normalize_summary(summary):
         summary += '.'
 
     return summary
+
+
+def main(argv):
+    """Main entry point."""
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     version=__version__)
+    parser.add_argument('--in-place', action='store_true',
+                        help='make changes to file instead of printing diff')
+    parser.add_argument('--no-backup', dest='backup', action='store_false',
+                        help='do not write backup files')
+    parser.add_argument('files', nargs='+',
+                        help='files to format')
+
+    args = parser.parse_args(argv)
+
+    for filename in args.files:
+        with open(filename) as input_file:
+            source = input_file.read()
+            formatted_source = format_code(source)
+
+        if args.in_place:
+            if args.backup:
+                with open(filename + '.backup', 'w') as backup_file:
+                    backup_file.write(source)
+
+            with open(filename, 'w') as output_file:
+                output_file.write(formatted_source)
+        else:
+            import difflib
+            diff = difflib.unified_diff([l + '\n' for l in source.split('\n')],
+                                        [l + '\n' for l in formatted_source.split('\n')],
+                                        'before/' + filename,
+                                        'after/' + filename)
+            sys.stdout.write(''.join(diff))
