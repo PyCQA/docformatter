b2a6e8a8352dc88c525ce414d2d91e5a8516aed1
Doyle Rowland <doyle.rowland@reliaqual.com>
2023-02-15 23:28:53 -0500

    chore: update version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'docformatter'
 copyright = '2022, Steven Myint'
 author = 'Steven Myint'
-release = '1.6.0-rc3'
+release = '1.6.0-rc4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
