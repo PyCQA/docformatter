34808ddfc737edcdbd8cf6964da92840b31de496
Doyle Rowland <doyle.rowland@reliaqual.com>
2023-04-28 12:36:39 -0400

    chore: update version strings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'docformatter'
 copyright = '2022-2023, Steven Myint'
 author = 'Steven Myint'
-release = '1.6.4'
+release = '1.7.0-rc1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
