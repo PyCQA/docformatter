5e1e0d5371dd6496d103eca713ba054a2d9f4805
Doyle Rowland <doyle.rowland@reliaqual.com>
2023-06-22 11:06:41 -0400

    chore: update GH workflows to cut pre-releases

--- src/docformatter/syntax.py
+++ src/docformatter/syntax.py
@@ -21,7 +21,7 @@
 # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""This module provides docformatter's Syntaxor class."""
+"""This module provides docformatter's syntax functions."""
 
 
 # Standard Library Imports
